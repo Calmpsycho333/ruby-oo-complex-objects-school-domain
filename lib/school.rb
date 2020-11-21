22bfbb238e2ad0876040419560f21f0291846fef
calmpsycho333 <wrathking75@yahoo.com>
2020-11-21 14:03:44 -0500

    commit tree 5f3b459e086cc7f5fe3d41ea0c26d6453da197a8

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster=Hash.new([])
+    end 
+    def add_student(name, age)
+        @roster[age]+=[name]
+        
+    end
+    def grade(num)
+        @roster[num]
+    end
+    def sort
+        new_roster = {}
+        @roster.each do |namey, val|
+           new_roster[namey] = val.sort
+            
+        end
+        new_roster
+    end
+end
